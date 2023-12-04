aa032929c70d45d8e35e08a7302b7def6ed8eaa3
unknown <solomon.schuster01@gmail.com>
2023-12-03 17:47:31 -0700

    solves part 1

--- 2/main.go
+++ 2/main.go
@@ -1,5 +1,66 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	println("Hello, World!")
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	sum := 0
+
+	for scanner.Scan() {
+		ok, id := isPossibleGame(scanner.Text())
+		if ok {
+			sum += id
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func isPossibleGame(s string) (bool, int) {
+	gameId := strings.Split(strings.Split(s, ":")[0], " ")[1]
+	fmt.Println(gameId)
+
+	maxes := map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+
+	sets := strings.Split(strings.Split(s, ":")[1], ";")
+
+	for _, set := range sets {
+		cubes := strings.Split(set, ",")
+
+		for _, cube := range cubes {
+			cube = strings.Trim(cube, " ")
+			num, err := strconv.Atoi(strings.Split(cube, " ")[0])
+			if err != nil {
+				panic(err)
+			}
+			color := strings.Split(cube, " ")[1]
+
+			if num > maxes[color] {
+				return false, 0
+			}
+		}
+	}
+
+	id, err := strconv.Atoi(gameId)
+	if err != nil {
+		panic(err)
+	}
+
+	return true, id
 }
