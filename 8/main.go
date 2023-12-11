9831090a6d293ceb5b686652b5cf9dc4beaff12e
Solomon Schuster <90484793+Solly922@users.noreply.github.com>
2023-12-10 21:41:50 -0700

    adds input.txt

--- 8/main.go
+++ 8/main.go
@@ -8,20 +8,20 @@ import (
 )
 
 func main() {
-	file, err := os.Open("test1.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
 	}
 
 	scanner := bufio.NewScanner(file)
 
-	m, ins, dest := buildMap(scanner)
-	fmt.Println(m, ins)
-	steps := countSteps(m, ins, dest)
+	m, ins, start, dest := buildMap(scanner)
+	// fmt.Println(m, ins)
+	steps := countSteps(m, ins, start, dest)
 	fmt.Println(steps)
 }
 
-func buildMap(s *bufio.Scanner) (quest map[string][2]string, instructions string, destination string) {
+func buildMap(s *bufio.Scanner) (quest map[string][2]string, instructions string, start string, destination string) {
 	m := make(map[string][2]string)
 	var d string
 	var dest string
@@ -29,7 +29,6 @@ func buildMap(s *bufio.Scanner) (quest map[string][2]string, instructions string
 	for s.Scan() {
 		split := strings.Split(s.Text(), "=")
 		if len(split) < 2 {
-			fmt.Println("Split?", split)
 			if len(split) == 1 && split[0] != "" {
 				d = split[0]
 			}
@@ -37,6 +36,9 @@ func buildMap(s *bufio.Scanner) (quest map[string][2]string, instructions string
 		}
 
 		node := strings.Trim(split[0], " ")
+		if start == "" {
+			start = node
+		}
 		options := strings.Split(split[1], ",")
 		left := options[0][2:]
 		right := options[1][1 : len(options[1])-1]
@@ -45,26 +47,26 @@ func buildMap(s *bufio.Scanner) (quest map[string][2]string, instructions string
 		dest = node
 	}
 
-	return m, d, dest
+	return m, d, start, dest
 }
 
-func countSteps(m map[string][2]string, ins string, dest string) int {
+func countSteps(m map[string][2]string, ins string, start string, dest string) int {
 	end := false
 	steps := 0
-	node := "AAA"
+	node := start
 	if ins == "" {
 		fmt.Println("No instructions")
 		return 0
 	}
-	fmt.Println(m)
+	fmt.Println(node, dest)
 
 	for !end {
-		if steps > 90 {
+		if steps > 999999 {
 			fmt.Println("Too many steps")
 			end = true
 		}
 		for _, v := range ins {
-			// fmt.Println(node, m[node])
+			fmt.Println(node, string(v))
 			if node == dest {
 				return steps
 			}
