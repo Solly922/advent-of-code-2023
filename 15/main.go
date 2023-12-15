9d78a388d2602263ab6820220a64224c44dea224
Solly922 <solomon.schuster01@gmail.com>
2023-12-15 16:06:49 -0700

    solves day 15 part 1

--- 15/main.go
+++ 15/main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
-	"bufio"
+	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+	file, err := os.ReadFile("input.txt")
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
+	codes := strings.Split(string(file), ",")
+
+	sum := sumHashes(codes)
+	fmt.Println(sum)
+}
+
+func hashCode(code string) int {
+	result := 0
+
+	for _, c := range code {
+		result += int(c)
+		result *= 17
+		result = result % 256
+	}
+
+	return result
+}
+
+func sumHashes(codes []string) int {
+	result := 0
+
+	for _, code := range codes {
+		result += hashCode(code)
+	}
+
+	return result
 }
