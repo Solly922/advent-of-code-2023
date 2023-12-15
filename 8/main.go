6a4496b0cc73370935ed884ee4ce06b5199d6af5
Solly922 <solomon.schuster01@gmail.com>
2023-12-15 15:46:15 -0700

    solves part 2 day 8

--- 8/main.go
+++ 8/main.go
@@ -9,20 +9,28 @@ import (
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+	file, err := os.Open("test2.txt")
 	if err != nil {
 		panic(err)
 	}
 
 	scanner := bufio.NewScanner(file)
 
-	startTime := time.Now()
 	m, ins, start, dest := buildMap(scanner)
+	startTime := time.Now()
 	// fmt.Println(m, ins)
 	steps := countSteps(m, ins, start, dest)
 	endTime := time.Now()
 	fmt.Println("Time taken:", endTime.Sub(startTime))
 	fmt.Println(steps)
+
+	startTime = time.Now()
+	aNodes := getANodes(m)
+	allSteps := countAllSteps(m, ins, aNodes)
+	lcm := lowestCommonMultiple(allSteps)
+	endTime = time.Now()
+	fmt.Println(lcm)
+	fmt.Println("Part 2 Time taken:", endTime.Sub(startTime))
 }
 
 func buildMap(s *bufio.Scanner) (quest map[string][2]string, instructions string, start string, destination string) {
@@ -66,7 +74,7 @@ func countSteps(m map[string][2]string, ins string, start string, dest string) i
 
 	for !end {
 		if steps > 999999 {
-			fmt.Println("Too many steps")
+			fmt.Println("Too many steps in countSteps")
 			end = true
 		}
 		for _, v := range ins {
@@ -86,4 +94,77 @@ func countSteps(m map[string][2]string, ins string, start string, dest string) i
 	}
 
 	return steps
+}
+
+func getANodes(m map[string][2]string) []string {
+	var result []string
+
+	for k, _ := range m {
+		if k[2] == 'A' {
+			result = append(result, k)
+		}
+	}
+
+	return result
+}
+
+func countStepsModif(m map[string][2]string, ins string, start string) int {
+	end := false
+	steps := 0
+	node := start
+	if ins == "" {
+		fmt.Println("No instructions")
+		return 0
+	}
+
+	for !end {
+		if steps > 999999 {
+			fmt.Println("Too many steps in countStepsModif")
+			end = true
+		}
+		for _, v := range ins {
+			// fmt.Println(node, string(v))
+			if node[2] == 'Z' {
+				return steps
+			}
+
+			if string(v) == "L" {
+				node = m[node][0]
+			}
+			if string(v) == "R" {
+				node = m[node][1]
+			}
+			steps++
+		}
+	}
+
+	return steps
+}
+
+func countAllSteps(m map[string][2]string, ins string, aNodes []string) []int {
+	var result []int
+
+	for _, v := range aNodes {
+		result = append(result, countStepsModif(m, ins, v))
+	}
+
+	return result
+}
+
+func gcd(a, b int) int {
+	if a == 0 {
+		return b
+	}
+
+	return gcd(b%a, a)
+}
+
+func lowestCommonMultiple(nums []int) int {
+	result := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		result = (result * nums[i]) / gcd(result, nums[i])
+	}
+
+	return result
 }
